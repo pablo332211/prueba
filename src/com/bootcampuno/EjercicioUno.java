9215c797dfcdf2d8036d572132b08a5ada2b4f99
pablo332211 <penoguera@misena.edu.co>
2022-08-27 02:50:27 -0500

    Se agrega la tarea

---
+++ src/com/bootcampuno/EjercicioUno.java
@@ -0,0 +1,40 @@
+package com.bootcampuno;
+
+public class EjercicioUno {
+
+    public static void main(String[] args){
+
+
+
+        int a = 12;
+        long b = 45645;
+        boolean c = 5>6;
+        double d = 35.4;
+        byte e = 5;
+        short f = 84;
+        long g = 564;
+        float h = 5.5f;
+
+        String palabra = "cadena";
+        char unal = 'a';
+
+        System.out.println("Los datos de Java son: ");
+
+        System.out.println("dato int = " + a);
+        System.out.println("dato long = " +b);
+        System.out.println("dato Bolean = " +c);
+        System.out.println("dato Double = " + d);
+        System.out.println("dato byte = " + e);
+        System.out.println("dato short = " + f);
+        System.out.println("dato long = " + g);
+        System.out.println("dato float = " + h);
+        System.out.println("dato String = " + palabra);
+        System.out.println("dato carater = " + unal);
+
+
+
+
+
+
+    }
+}
